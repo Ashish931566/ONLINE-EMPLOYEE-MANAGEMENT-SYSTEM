4cc3d3914f0a240862afd7e043e94b34abf48cb1
Ashish931566 <101096542+Ashish931566@users.noreply.github.com>
2025-09-10 12:39:00 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -528,6 +528,6 @@ def profile():
 
 # ------------------ Run ------------------
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=False)
